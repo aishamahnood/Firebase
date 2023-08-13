cd12858e5c25ecba73f11a2ca8290993bd7c4b0a
aishamahnood <121711108+aishamahnood@users.noreply.github.com>
2023-08-12 21:39:21 -0700

    Add files via upload
    Firebase Js Project

---
+++ Figma Website/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCyUBvBs7YtfvjXhY-tlRjwGhTR7-BzrhQ",
+    authDomain: "smit-web-project.firebaseapp.com",
+    projectId: "smit-web-project",
+    storageBucket: "smit-web-project.appspot.com",
+    messagingSenderId: "478414190520",
+    appId: "1:478414190520:web:6a14a06e061a7f73b9414d",
+    measurementId: "G-CXL3H7MWF9"
+  };
